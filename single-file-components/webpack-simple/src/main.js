0d133e8e48274c5bf59bc081b9d04ad81599ed0f
redsky <hyun0238@gmail.com>
2018-12-18 17:29:56 +0900

    파일수정

--- single-file-components/webpack-simple/src/main.js
+++ single-file-components/webpack-simple/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Global Components
+import NavHeader from './components/NavHeader.vue';
+Vue.component('nav-header', NavHeader);
+
 new Vue({
   el: '#app',
   render: h => h(App)
